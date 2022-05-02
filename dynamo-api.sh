63ea96097de411c31e947288f3d8802285bf8812
robscott1 <47842575+robscott1@users.noreply.github.com>
2022-05-02 10:26:01 -0700

    Delete dynamo-api.sh

--- dynamo-api.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-KEY=$1
-NAME=$2
-
-curl https://nltlfq9zc0.execute-api.us-east-1.amazonaws.com/addItem  -X POST -H 'Content-Type: application/json' -d '{"itemKey": "'"$KEY"'", "name": "'"$NAME"'"}' 
